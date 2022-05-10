5eee865426ef2e47414e805593e7d6507ea20eb0
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2022-05-10 09:06:14 +0800

    RMX2040: Define

--- twrp_RMX2040.mk
+++ twrp_RMX2040.mk
@@ -28,7 +28,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
